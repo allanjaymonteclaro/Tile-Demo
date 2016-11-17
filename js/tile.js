7d6b8a85103f7f17a07a60d826d1520420274750
Allan Jay <allanjaymonteclaro@gmail.com>
2016-11-17 15:16:30 +0800

    First exercise done

--- js/tile.js
+++ js/tile.js
@@ -1,7 +1,16 @@
 $("input[type='checkbox']").change(function(){
-    if($(this).is(":checked")){
-        $(this).parent().addClass("correctBackground"); 
-    }else{
-        $(this).parent().removeClass("correctBackground");  
+	if ($(this).hasClass("correct")) {
+		if($(this).is(":checked")){
+       		$(this).parent().addClass("correctBackground"); 
+    	}else{
+        	$(this).parent().removeClass("correctBackground");  
+    	}
+	}
+    else{
+    	if($(this).is(":checked")){
+       		$(this).parent().addClass("wrongBackground"); 
+    	}else{
+        	$(this).parent().removeClass("wrongBackground");  
+    	}
     }
 });
